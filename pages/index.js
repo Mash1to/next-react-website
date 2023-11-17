f058112072645e79479cf63c0aef4c3c5b07c677
Mash1to <hayato.m8812@au.com>
2023-11-17 16:35:20 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,42 +2,20 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Hero from '../components/hero'
+import Layout from 'components/layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <header>HEADER</header>
+    <Layout>
+        <Hero/>
+    </Layout>
       
-      <main>
-        <div className={styles.container}>
-          <h1>CUBE</h1>
-          <p>アウトプットしていくサイト</p>
-        </div>
-      </main>
       
-      <footer>FOOTER</footer>
-    </>
-  )
-}
-
-function EachPost() {
-  return (
-    <article>
-      <a href="post.html">
-        <h3>記事のタイトル</h3>
-      </a>
-    </article>
-  )
-}
-
-export default function Posts() {
-  return (
-    <section>
-      <h2>おすすめ記事</h2>
-      <EachPost />
-      <EachPost />
-    </section>
+      
+      
+    
   )
 }
