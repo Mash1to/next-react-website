ea4120579e2953765ad441a7b536e9b91bf81b91
Mash1to <hayato.m8812@au.com>
2023-11-24 16:30:13 +0900

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,11 @@
-export default function Hero() {
+import style from 'styles/hero.module.css'
+
+export default function Hero({ title, subtitle, imageOn = false, imageOff = false}) {
     return (
         <div>
-            <h1>CUBE</h1>
-            <p>アウトプットしていくサイト</p>
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+            {imageOn && <figure> [画像] </figure>}
         </div>
     )
 }
