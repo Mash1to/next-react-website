639e04db411650f1a8bdf32fc979c809269080ba
Mash1to <hayato.m8812@au.com>
2023-11-24 16:30:11 +0900

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,3 +1,12 @@
+import Logo from './logo'
+import Nav from 'components/nav'
+import styles from 'styles/header.module.css'
+
 export default function Header() {
-    return <header>HEADER</header>
+    return (
+        <div className={styles.flexContainer}>
+            <Logo boxOn/>
+            <Nav />
+        </div>
+    )
 }
