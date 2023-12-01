b91035907283c666b4abddb76bd7e0365ded16ed
Mash1to <hayato.m8812@au.com>
2023-12-01 16:31:10 +0900

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -1,10 +1,12 @@
 import style from 'styles/hero.module.css'
 
-export default function Hero({ title, subtitle, imageOn = false, imageOff = false}) {
+export default function Hero({ title, subtitle, imageOn = false }) {
     return (
-        <div>
-            <h1>{title}</h1>
-            <p>{subtitle}</p>
+        <div className={style.flexContainer}>
+            <div className={style.text}>
+                <h1 className={style.title}>{title}</h1>
+                <p className={style.subtitle}>{subtitle}</p>
+            </div>
             {imageOn && <figure> [画像] </figure>}
         </div>
     )
