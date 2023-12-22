e8e2c7bddc3e523410f43207db174599eb0234a1
Mash1to <hayato.m8812@au.com>
2023-12-22 16:38:45 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import Meta from 'components/meta';
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/home.module.css'
+import styles from 'styles/home.module.css'
 import Container from 'components/container'
 import Hero from '../components/hero'
 
@@ -10,6 +11,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <Container>
+      <Meta />
       <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
     </Container>
   )
