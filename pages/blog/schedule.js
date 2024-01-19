0f187fe94ae155fc901c1a7e2b488824ae53e912
Mash1to <hayato.m8812@au.com>
2024-01-19 16:32:35 +0900

    Update schedule.js

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,13 +1,50 @@
-export default function Schedule() {
-    return <h1>記事のタイトル</h1>
+import { getPostBySlug } from 'lib/api'
+import Container from 'components/container'
+import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
+import Image from 'next/image'
+
+export default function Schedule({
+    title,
+    publish,
+    content,
+    eyecatch,
+    categories,
+})   {
+    return (
+        <Container>
+            <article>
+                <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+                
+                <figure>
+                    <Image
+                        src={eyecatch.url}
+                        alt=""
+                        layout="responsive"
+                        width={eyecatch.width} 
+                        height={eyecatch.height}
+                        sizes="(min-width: 1152px) 1152px, 100vw"
+                        priority
+                    />
+                </figure>
+            </article>
+        </Container>
+    )
 }
 
 export async function getStaticProps() {
-    console.log('処理１')
-    setTimeout(() => console.log('処理２'), 1000)
-    console.log('処理３')
-
+    const slug = 'schedule'
+    
+    const post = await getPostBySlug(slug)
+    
     return {
-        props: {},
+        props: {
+            title: post.title,
+            publish: post.publishDate,
+            content: post.content,
+            eyecatch: post.eyecatch,
+            categories: post.categories,
+        },
     }
 }
